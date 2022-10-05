1b7d230ffcac98718c7038700a86a1307bd15570
Fernanda Pisconte <fpisconte@berkeley.edu>
2022-10-05 04:36:51 +0000

    aaa

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,12 +2,4 @@ class Movie < ActiveRecord::Base
   def self.return_ratings
     ['G','PG','PG-13','R']
   end
-
-  def self.checked_ratings(params)
-    if params.nil?
-      []
-    else
-      params.keys
-    end
-  end
 end
