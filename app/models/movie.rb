2ae47da6960d7dc88ce4c099fb7d7eef54aac841
Fernanda Pisconte <fpisconte@berkeley.edu>
2022-10-05 04:46:00 +0000

    fixed bug

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -2,4 +2,12 @@ class Movie < ActiveRecord::Base
   def self.return_ratings
     ['G','PG','PG-13','R']
   end
+
+  def self.checked_ratings ratings:
+    if ratings.empty?
+      ['G','PG','PG-13','R']
+    else
+      ratings.keys
+    end
+  end
 end
